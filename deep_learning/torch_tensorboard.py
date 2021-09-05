20f8d0470683760d7509bec5cfda4af1d24497f8
lsh67890 <68392535+lsh67890@users.noreply.github.com>
2021-09-05 21:00:44 +0100

    Add files via upload

---
+++ deep_learning/torch_tensorboard.py
@@ -0,0 +1,140 @@
+import os
+from glob import glob
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from PIL import Image
+
+seed = 1
+lr = 0.001
+momentum = 0.5
+batch_size = 64
+test_batch_size = 64
+epochs = 5
+no_cuda = False
+log_interval = 100
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+# preprocess
+train_paths = glob('dataset/mnist_png/training/*/*.png')[:1000]
+test_paths = glob('dataset/mnist_png/testing/*/*.png')[:1000]
+print(len(train_paths), len(test_paths))
+
+class Dataset(Dataset):
+    def __init__(self, data_paths, transform=None):
+        self.data_paths = data_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data_paths)
+
+    def __getitem__(self, idx):
+        path = self.data_paths[idx]
+        img = Image.open(path).convert('L')
+        label = int(path.split('\\')[-2])
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+torch.manual_seed(seed)
+use_cuda = not no_cuda and torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+train_loader = torch.utils.data.DataLoader(
+    Dataset(train_paths,
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.406],
+                    std=[0.225])])
+            ),
+    batch_size=batch_size,
+    shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    Dataset(test_paths,
+            transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.406],
+                    std=[0.224])])
+            ),
+    batch_size=batch_size,
+    shuffle=False
+)
+
+# optimization
+model = Net().to(device)
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+# training
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter()
+
+for epoch in range(1, epochs+1):
+    # train mode
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train epoch: {} [{}/{}] {:.0f}% Loss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()
+            ))
+    # test mode
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get idx of max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} {:.0f}%'.format(
+        test_loss, correct, len(test_loader.dataset), accuracy
+    ))
+
+    # tensorboard
+    if epoch == 0:
+        grid = torchvision.utils.make_grid(data) # this data is from the train loader
+        writer.add_image('images', grid, epoch)
+        writer.add_graph(model, data)
+
+    writer.add_scalar('Loss/train/', loss, epoch)
+    writer.add_scalar('Loss/test/', test_loss, epoch)
+    writer.add_scalar('Accuracy/test/', accuracy, epoch)
